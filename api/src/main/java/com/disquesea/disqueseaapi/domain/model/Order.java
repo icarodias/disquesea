39a5ccb0e7bfbe648455dd607b2c754b6f17c8c3
Icaro Dias <icarodiasdev@gmail.com>
2023-05-21 19:26:54 -0300

    adding Order entity

---
+++ api/src/main/java/com/disquesea/disqueseaapi/domain/model/Order.java
@@ -0,0 +1,50 @@
+package com.disquesea.disqueseaapi.domain.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_orders")
+@RequiredArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    private BigDecimal price;
+
+    private LocalDate date;
+
+    private Boolean isSell;
+
+    private String description;
+
+    @ManyToOne
+    private Product product;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        return id.equals(order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
