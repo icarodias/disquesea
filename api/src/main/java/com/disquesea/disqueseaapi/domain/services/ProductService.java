b8e0b68a64b145c4068492ceaa6047d932bcfd83
Icaro Dias <icarodiasdev@gmail.com>
2023-05-22 14:33:56 -0300

    improving find all products to accept specification filters

--- api/src/main/java/com/disquesea/disqueseaapi/domain/services/ProductService.java
+++ api/src/main/java/com/disquesea/disqueseaapi/domain/services/ProductService.java
@@ -2,10 +2,13 @@ package com.disquesea.disqueseaapi.domain.services;
 
 import com.disquesea.disqueseaapi.domain.model.Product;
 import com.disquesea.disqueseaapi.domain.respositories.ProductRepository;
+import com.disquesea.disqueseaapi.specifications.ProductSpecification;
+import com.disquesea.disqueseaapi.specifications.dto.ProductCriteriaDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,8 +17,15 @@ public class ProductService {
 
     private final ProductRepository repository;
 
-    public Page<Product> findAll(Pageable pageable) {
-        return repository.findAll(pageable);
+    public Page<Product> findAll(ProductCriteriaDTO criteriaDTO, Pageable pageable) {
+        Specification<Product> specification = Specification
+                .where(ProductSpecification.nameContains(criteriaDTO.getName()))
+                .and(ProductSpecification.scaleIs(criteriaDTO.getScale()))
+                .and(ProductSpecification.statusIs(criteriaDTO.getStatus()))
+                .and(ProductSpecification.categoryIs(criteriaDTO.getCategory()))
+                .and(ProductSpecification.catalogVisibility(criteriaDTO.getIsVisibleInCatalog()));
+
+        return repository.findAll(specification, pageable);
     }
 
     public Product findById(long id) {
