a9d3477918f2a8a3716783ab9bdf049f2067cd0b
Icaro Dias <icarodiasdev@gmail.com>
2023-05-22 11:40:32 -0300

    adding findBYId method in WalletRepository

--- api/src/main/java/com/disquesea/disqueseaapi/domain/respositories/WalletRepository.java
+++ api/src/main/java/com/disquesea/disqueseaapi/domain/respositories/WalletRepository.java
@@ -4,7 +4,11 @@ import com.disquesea.disqueseaapi.domain.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface WalletRepository extends JpaRepository<Wallet, Long>, JpaSpecificationExecutor<Wallet> {
+
+    Optional<Wallet> findById(long id);
 }
 
 
