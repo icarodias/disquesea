3d43855515d48ae55cfa05f1825f7a2c9b7700d5
Icaro Dias <icarodiasdev@gmail.com>
2023-05-23 19:41:48 -0300

    reset wallet from post to put (it is idempotent)

--- api/src/main/java/com/disquesea/disqueseaapi/web/rest/controllers/WalletController.java
+++ api/src/main/java/com/disquesea/disqueseaapi/web/rest/controllers/WalletController.java
@@ -22,7 +22,7 @@ public class WalletController {
         return mapper.map(service.obtain());
     }
 
-    @PostMapping
+    @PutMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void reset() {
         service.reset();
