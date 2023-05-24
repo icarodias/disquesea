605410d0250b48dee08b64460913ddd6c04d5da7
Icaro Dias <icarodiasdev@gmail.com>
2023-05-24 09:34:44 -0300

    unique query to delete all orders

---
+++ api/src/main/java/com/disquesea/disqueseaapi/domain/respositories/helper/OrderRepositoryQueries.java
@@ -0,0 +1,6 @@
+package com.disquesea.disqueseaapi.domain.respositories.helper;
+
+public interface OrderRepositoryQueries {
+
+    void deleteAllOrders();
+}
--- api/src/main/java/com/disquesea/disqueseaapi/domain/services/OrderService.java
+++ api/src/main/java/com/disquesea/disqueseaapi/domain/services/OrderService.java
@@ -115,6 +115,6 @@ public class OrderService {
     }
 
     public void deleteAll() {
-        repository.deleteAll();
+        repository.deleteAllOrders();
     }
 }
