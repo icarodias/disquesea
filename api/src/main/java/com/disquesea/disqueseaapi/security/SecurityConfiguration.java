4655bca7c34211306f7463d079c2037af15f7e39
Icaro Dias <icarodiasdev@gmail.com>
2023-05-26 21:07:53 -0300

    configuring SecurityFilterChain bean

---
+++ api/src/main/java/com/disquesea/disqueseaapi/security/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.disquesea.disqueseaapi.security;
+
+import com.disquesea.disqueseaapi.security.filter.UserFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final String[] ALL_ROLES = {"ADMIN", "MANAGER","EMPLOYEE"};
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable());
+
+        http.authorizeHttpRequests(
+                (authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/products").hasAnyRole(ALL_ROLES)
+                        .requestMatchers(HttpMethod.POST, "/products").hasAnyRole("ADMIN", "MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/products/*").hasAnyRole("ADMIN", "MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/products/*").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/orders").hasAnyRole(ALL_ROLES)
+                        .requestMatchers(HttpMethod.POST, "/orders").hasAnyRole(ALL_ROLES)
+                        .requestMatchers(HttpMethod.DELETE, "/orders").hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/wallet/reset").hasAnyRole(ALL_ROLES)
+                        .requestMatchers(HttpMethod.PUT, "/wallet/reset").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        http.cors(Customizer.withDefaults());
+
+        http.addFilterBefore(new UserFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
