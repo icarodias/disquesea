5b6866fd4cf8bfad473dc50eb1ba8da36f297b7d
Icaro Dias <icarodiasdev@gmail.com>
2023-05-26 22:18:26 -0300

    adding a initial script documentation

---
+++ api/documentation/initial_script.sql
@@ -0,0 +1,5 @@
+-- This script defines the wallet value to 0.00 and set the first user ADMIN
+
+INSERT INTO tb_wallet (id, value) VALUES (1, 0.00);
+
+INSERT INTO disquesea.tb_users (id, username, password, name, `role`) VALUES (1, 'icaro', '$2a$10$Jy.YeFtUCzlvC1a2xt5sC.RFfXzsCH2Tn63C9trkt3qb9VxUY7crO', 'Icaro', 'ADMIN');
