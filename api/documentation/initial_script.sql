8b99d6dcc60e84f3e1046f2501a86dcc663da1f9
Icaro Dias <icarodiasdev@gmail.com>
2023-06-01 18:12:46 -0300

    improving initial_script

--- api/documentation/initial_script.sql
+++ api/documentation/initial_script.sql
@@ -2,4 +2,4 @@
 
 INSERT INTO tb_wallet (id, value) VALUES (1, 0.00);
 
-INSERT INTO disquesea.tb_users (id, username, password, name, `role`) VALUES (1, 'icaro', '$2a$10$Jy.YeFtUCzlvC1a2xt5sC.RFfXzsCH2Tn63C9trkt3qb9VxUY7crO', 'Icaro', 'ADMIN');
+INSERT INTO disquesea.tb_users (id, username, password, name, role) VALUES (1, 'icaro', '$2a$10$Jy.YeFtUCzlvC1a2xt5sC.RFfXzsCH2Tn63C9trkt3qb9VxUY7crO', 'Icaro', 'ADMIN');
