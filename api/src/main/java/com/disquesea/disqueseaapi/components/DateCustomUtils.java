0ac8e3e01f74220eeb644eece331a07058d3a089
Icaro Dias <icarodiasdev@gmail.com>
2023-06-11 14:49:52 -0300

    adding new DateTimeFormatter

--- api/src/main/java/com/disquesea/disqueseaapi/components/DateCustomUtils.java
+++ api/src/main/java/com/disquesea/disqueseaapi/components/DateCustomUtils.java
@@ -6,10 +6,11 @@ import java.time.format.DateTimeFormatter;
 import static java.util.Objects.isNull;
 
 public abstract class DateCustomUtils {
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public static final DateTimeFormatter DATE_FILE_NAME_FORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     public static LocalDate fromString(String stringDate) {
-
         return isNull(stringDate) ? null : LocalDate.parse(stringDate, DATE_FORMAT);
     }
 
