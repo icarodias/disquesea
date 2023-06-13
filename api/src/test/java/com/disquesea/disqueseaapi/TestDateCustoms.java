cfed1b2331c86ceac39a1acd932c57f990fa395b
Icaro Dias <icarodiasdev@gmail.com>
2023-06-13 00:13:21 -0300

    improving test DateFormatTest

--- api/src/test/java/com/disquesea/disqueseaapi/TestDateCustoms.java
+++ api/src/test/java/com/disquesea/disqueseaapi/TestDateCustoms.java
@@ -1,6 +1,6 @@
 package com.disquesea.disqueseaapi;
 
-import com.disquesea.disqueseaapi.components.DateCustomUtils;
+import com.disquesea.disqueseaapi.components.DateCustomComponent;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +9,8 @@ import java.time.LocalDate;
 public class TestDateCustoms {
     @Test
     public void DateFormatTest() {
-        Assertions.assertEquals(LocalDate.now().format(DateCustomUtils.DATE_FILE_NAME_FORMAT).toString(), "06-06-2023");
+        LocalDate date = LocalDate.of(2023, 02, 18);
+        Assertions.assertEquals(date.format(DateCustomComponent.DATE_FILE_NAME_FORMAT).toString(), "18-02-2023");
+        Assertions.assertEquals(date.format(DateCustomComponent.DATE_FORMAT).toString(), "18/02/2023");
     }
 }
