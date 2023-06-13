9ba7a5d653e907fc451bf5260385febd21cf31d2
Icaro Dias <icarodiasdev@gmail.com>
2023-06-13 12:12:48 -0300

    improving space format

--- api/src/main/resources/liquibase/changelog.sql
+++ api/src/main/resources/liquibase/changelog.sql
@@ -3,38 +3,38 @@
 -- changeset icaro-dias:create-table-products
 
 create table tb_products (
-	id bigint not null auto_increment,
-	name varchar(30) not null unique,
-	amount decimal(38,2) not null,
-	price decimal(38,2) not null,
-	is_visible_in_catalog bit not null,
-	category enum ('BIRD','FISH','MEAT','OTHER','SEAFOOD','SHRIMP','SNACK'),
-	status enum ('ABSENT','AVAILABLE','CRITICAL'),
-	product_scale enum('KILOGRAM', 'UNIT'),
+    id bigint not null auto_increment,
+    name varchar(30) not null unique,
+    amount decimal(38,2) not null,
+    price decimal(38,2) not null,
+    is_visible_in_catalog bit not null,
+    category enum ('BIRD','FISH','MEAT','OTHER','SEAFOOD','SHRIMP','SNACK'),
+    status enum ('ABSENT','AVAILABLE','CRITICAL'),
+    product_scale enum('KILOGRAM', 'UNIT'),
 
-	primary key (id)
+    primary key (id)
 ) engine = InnoDB;
 
 -- changeset icaro-dias:create-table-orders
 
 create table tb_orders (
-	id bigint not null auto_increment,
-	amount decimal(38,2) not null,
-	price decimal(38,2) not null,
+    id bigint not null auto_increment,
+    amount decimal(38,2) not null,
+    price decimal(38,2) not null,
     is_sell bit not null,
     created_at date not null,
     product_id bigint not null,
 
-	primary key (id),
+    primary key (id),
     constraint fk_order_product foreign key (product_id) references tb_products (id)
 ) engine = InnoDB;
 
 -- changeset icaro-dias:create-table-wallet
 
 create table tb_wallet (
-	id bigint not null auto_increment,
-	value decimal(38,2) not null,
-	
+    id bigint not null auto_increment,
+    value decimal(38,2) not null,
+
     primary key (id)
 ) engine = InnoDB;
 
@@ -43,7 +43,7 @@ create table tb_wallet (
 create table tb_users (
     id bigint not null auto_increment,
     username varchar(20) not null unique,
-    name varchar(40) not null, 
+    name varchar(40) not null,
     password varchar(60) not null,
     role enum ('ADMIN', 'MANAGER', 'EMPLOYEE'),
 
