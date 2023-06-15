6ec43caf5765a5c33871d74496deee9749068f8a
Icaro Dias <icarodiasdev@gmail.com>
2023-06-15 18:16:01 -0300

    adding tag name and description in endpoints

--- api/src/main/java/com/disquesea/disqueseaapi/controllers/UserController.java
+++ api/src/main/java/com/disquesea/disqueseaapi/controllers/UserController.java
@@ -1,10 +1,12 @@
 package com.disquesea.disqueseaapi.controllers;
 
+import com.disquesea.disqueseaapi.controllers.annotation.OperationAuth;
 import com.disquesea.disqueseaapi.domain.model.User;
 import com.disquesea.disqueseaapi.domain.services.UserService;
 import com.disquesea.disqueseaapi.controllers.dtos.requests.CreateUserDTO;
 import com.disquesea.disqueseaapi.controllers.dtos.responses.UserResponseDTO;
 import com.disquesea.disqueseaapi.controllers.mappers.UserMapper;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,14 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Tag(name = "User API")
 public class UserController {
 
     private final UserService service;
 
     private final UserMapper mapper;
 
-
     @PostMapping
+    @OperationAuth(summary = "Create User")
     public UserResponseDTO create(@RequestBody @Valid CreateUserDTO createUserDTO) {
         final User user = mapper.toDomain(createUserDTO);
 
--- api/src/main/java/com/disquesea/disqueseaapi/controllers/WalletController.java
+++ api/src/main/java/com/disquesea/disqueseaapi/controllers/WalletController.java
@@ -1,14 +1,17 @@
 package com.disquesea.disqueseaapi.controllers;
 
+import com.disquesea.disqueseaapi.controllers.annotation.OperationAuth;
 import com.disquesea.disqueseaapi.domain.services.WalletService;
 import com.disquesea.disqueseaapi.controllers.dtos.responses.WalletResponseDTO;
 import com.disquesea.disqueseaapi.controllers.mappers.WalletMapper;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/wallet")
+@Tag(name = "Wallet API")
 @RequiredArgsConstructor
 public class WalletController {
 
@@ -17,12 +20,14 @@ public class WalletController {
     private final WalletMapper mapper;
 
     @GetMapping
+    @OperationAuth(summary = "Get Wallet content")
     @ResponseStatus(HttpStatus.OK)
     public WalletResponseDTO obtain() {
         return mapper.map(service.obtain());
     }
 
     @PutMapping("/reset")
+    @OperationAuth(summary = "Reset Wallet content")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void reset() {
         service.reset();
